23386672b28c56863e027250f324d254cf97befe
Jim <jim.brooke@cern.ch>
2022-01-13 15:53:10 +0000

    add setup file

---
+++ setup.sh
@@ -0,0 +1,6 @@
+source /cvmfs/sft.cern.ch/lcg/views/LCG_99/x86_64-centos7-gcc10-opt/setup.sh
+
+DELPHES_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+export PATH=$PATH:$DELPHES_DIR/install/bin
+
