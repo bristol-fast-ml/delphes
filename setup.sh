2416ca05ce5fce446d7a32c157f09f7309566df1
Jim <jim.brooke@cern.ch>
2022-03-22 19:58:07 +0000

    change path order

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ source /cvmfs/sft.cern.ch/lcg/views/LCG_99/x86_64-centos7-gcc10-opt/setup.sh
 
 DELPHES_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-export PATH=$PATH:$DELPHES_DIR/install/bin
+export PATH=$DELPHES_DIR/install/bin:$PATH
 
